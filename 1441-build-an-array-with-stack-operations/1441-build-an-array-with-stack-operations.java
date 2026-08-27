b2e92d6a49ec48ad2e279f9c36e90a9fb0f07e51
saurabhty456 <saxenasaurabh086@gmail.com>
2026-08-27 22:06:56 +0530

    Time: 0 ms (100%), Space: 44.1 MB (71.2%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,27 +1,21 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String>s=new ArrayList<>();
-        Stack<Integer>st=new Stack<>();
-        for(int i=target.length-1;i>=0;i--)
-        {
-            st.push(target[i]);
-        }
-        int c=1;
-        while(!st.isEmpty())
-        {
-            if(st.peek()==c)
-            {
-                s.add("Push");
-                st.pop();
-                c++;
+        List<String> result = new ArrayList<>();
+        int currentNumber = 1;
+        
+        for (int num : target) {
+            // Push and Pop the missing numbers between currentNumber and num
+            while (currentNumber < num) {
+                result.add("Push");
+                result.add("Pop");
+                currentNumber++;
             }
-            else
-            {
-               s.add("Push");
-               s.add("Pop");
-               c++; 
-            }
+            // Push the matching number
+            result.add("Push");
+            currentNumber++;
         }
-        return s;
+        
+        return result;
     }
 }
+
