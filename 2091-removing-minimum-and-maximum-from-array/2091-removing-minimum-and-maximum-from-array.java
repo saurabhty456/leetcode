1d3633c179d6a714b7a23850111b7a346353152a
saurabhty456 <saxenasaurabh086@gmail.com>
2026-08-30 22:08:55 +0530

    Time: 3 ms (61.82%), Space: 86.8 MB (23.09%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,22 +1,25 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        int n=nums.length;
-        for(int i=0;i<nums.length;i++)
-        {
-            map.put(nums[i],i);
-            min=Math.min(nums[i],min);
-            max=Math.max(nums[i],max);
+        int n = nums.length;
+        int minIn = 0, maxIn = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < nums[minIn]) minIn = i;
+            if (nums[i] > nums[maxIn]) maxIn = i;
         }
-        int minIn=map.get(min);
-        int maxIn=map.get(max);
-        int i=Math.min(minIn,maxIn);
-        int j=Math.max(minIn,maxIn);
-        int f=j+1;
-        int b=n-i;
-        int fb=(i+1)+(n-j);
-        return Math.min(Math.min(f,b),fb);
+        
+        int i = Math.min(minIn, maxIn);
+        int j = Math.max(minIn, maxIn);
+        
+        // Case 1: Delete from front only
+        int front = j + 1;
+        
+        // Case 2: Delete from back only
+        int back = n - i;
+        
+        // Case 3: Delete from both ends
+        int both = (i + 1) + (n - j);
+        
+        return Math.min(Math.min(front, back), both);
     }
 }
