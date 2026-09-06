1effacdfbc0f1ace2441dda8befa7b736488205c
saurabhty456 <saxenasaurabh086@gmail.com>
2026-09-06 15:07:07 +0530

    Time: 5 ms (99.9%), Space: 85.1 MB (95.1%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,22 +1,48 @@
+// class Solution {
+//     public int maximumUniqueSubarray(int[] nums) {
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         int i=0;
+//         int j=0;
+//         int sum=0;
+//         int max=Integer.MIN_VALUE;
+//         for(j=0;j<nums.length;j++)
+//         {
+//             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+//             sum+=nums[j];
+//             while(map.get(nums[j])>1)
+//             {
+//                 map.put(nums[i],map.get(nums[i])-1);
+//                 sum-=nums[i];
+//                 i++;
+//             }
+//             max=Math.max(sum,max);
+//         }
+//         return max;
+//     }
+// }
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
+        int n = nums.length;
+        boolean[] set = new boolean[10001];
+
         int i=0;
-        int j=0;
-        int sum=0;
-        int max=Integer.MIN_VALUE;
-        for(j=0;j<nums.length;j++)
-        {
-            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            sum+=nums[j];
-            while(map.get(nums[j])>1)
-            {
-                map.put(nums[i],map.get(nums[i])-1);
-                sum-=nums[i];
+        int sum = 0;
+        int max = 0;
+
+        for(int j=0; j<n; j++){
+            sum += nums[j];
+
+            while(set[nums[j]]){
+                set[nums[i]] = false;
+                sum -= nums[i];
                 i++;
-            }
-            max=Math.max(sum,max);
+            } 
+
+            set[nums[j]] = true;;
+
+            max = Math.max(sum, max);
         }
+
         return max;
     }
 }
