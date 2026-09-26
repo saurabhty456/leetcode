3cca48554df3eeac262daf4ca571279e53a39edf
saurabhty456 <saxenasaurabh086@gmail.com>
2026-09-26 23:05:02 +0530

    Time: 3 ms (100%), Space: 175.7 MB (28.97%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,31 +1,52 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//        int posi[]=new int[nums.length/2];
+//        int negat[]=new int[nums.length/2];
+//        int r[]=new int[nums.length];
+//        int ni=0;
+//        int pos=0;
+//        for(int i=0;i<nums.length;i++)
+//        {
+//         if(nums[i]>=0)
+//         {
+//         posi[pos++]=nums[i];
+//         }
+//         else
+//         {
+//             negat[ni++]=nums[i];
+//         }
+//        }
+//        int p=0;
+//        int n=0;
+//        int m=0;
+//        while(p<posi.length&&n<posi.length)
+//        {
+//         r[m++]=posi[p];
+//         r[m++]=negat[n];
+//         p++;
+//         n++;
+//        }
+//        return r; 
+//     }
+// }
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-       int posi[]=new int[nums.length/2];
-       int negat[]=new int[nums.length/2];
-       int r[]=new int[nums.length];
-       int ni=0;
-       int pos=0;
-       for(int i=0;i<nums.length;i++)
-       {
-        if(nums[i]>=0)
+        int p=0;
+        int n=1;
+        int r[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
         {
-        posi[pos++]=nums[i];
+            if(nums[i]>0)
+            {
+                r[p]=nums[i];
+                p+=2;
+            }
+            else
+            {
+                r[n]=nums[i];
+                n+=2;
+            }
         }
-        else
-        {
-            negat[ni++]=nums[i];
-        }
-       }
-       int p=0;
-       int n=0;
-       int m=0;
-       while(p<posi.length&&n<posi.length)
-       {
-        r[m++]=posi[p];
-        r[m++]=negat[n];
-        p++;
-        n++;
-       }
-       return r; 
+        return r;
     }
 }
